252d4eaf9eb9055dd8b2b1e383170993db587810
Sam Szuflita <sam.szuflita@gmail.com>
2020-10-19 09:48:24 -0400

    Hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize};
+
+// This `derive` requires the `serde` dependency.
+#[derive(Deserialize)]
+struct Ip {
+    origin: String,
 }
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let ip = reqwest::get("http://httpbin.org/ip")
+        .await?
+        .json::<Ip>()
+        .await?;
+
+    println!("ip: {}", ip.origin);
+
+    Ok(())
+}
+
